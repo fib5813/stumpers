e95e5e3395eb6f5938d4e9ec112776f85b7a976f
fib5813 <urvashi.bhavsar08@gmail.com>
2020-10-15 11:33:30 -0400

    Create max_area.cpp

---
+++ max_area.cpp
@@ -0,0 +1,11 @@
+  int maxArea(vector<int>& height) {
+        int start = 0;
+        int end = height.size()-1;
+    //                 out = new_out;
+    //             }
+    //             end++;
+    //         }
+    //         start++;
+    //     }
+    //     return out;
+    // }
